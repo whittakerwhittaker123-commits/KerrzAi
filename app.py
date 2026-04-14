2f9bddf7d0bbc7b16aef32151b58ca31d32ff61f
whittakerwhittaker123-commits <whittakerwhittaker123@gmail.com>
2026-04-14 15:15:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def signal():
+    signal = random.choice(["BUY", "SELL"])
+
+    entry = 42000
+
+    if signal == "BUY":
+        sl = entry - 1000
+        tp = entry + 2000
+    else:
+        sl = entry + 1000
+        tp = entry - 2000
+
+    return jsonify({
+        "signal": signal,
+        "entry": entry,
+        "sl": sl,
+        "tp": tp
+    })
